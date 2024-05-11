35cd1b7c7ff86555562c1e172fa04c3aadd63a4e
Edilson Holanda <edilson_holanda@hotmail.com>
2024-05-11 18:11:41 -0300

    exemplo de componente table em react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Box } from '@mui/material';
+import StatementEntries from './components/StatementEntries';
+
+function App() {
+  return (
+    <Box>
+      <StatementEntries />
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/components/StatementEntries/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+import Styles from './styles';
+
+const StatementEntries = () => {
+
+    const classes = Styles(); 
+    return (
+        <Box>
+            <Button>Teste</Button>
+        </Box>
+    )
+};
+
+export default StatementEntries;
+
